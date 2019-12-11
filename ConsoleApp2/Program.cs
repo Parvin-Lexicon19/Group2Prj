a351fa266146987571515a240e1f01884ba23319
Radhika Jayaraman <radhika_jram@outlook.com>
2019-12-11 10:36:04 +0100

    Confiict Resolved push

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,7 +7,11 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+<<<<<<< HEAD
+            Console.WriteLine("Hello from Radhika To Test!!");
+=======
             Console.WriteLine("Hello from Parvin !!");
+>>>>>>> ff89f4f... ConflicFromParvin
         }
     }
 }
